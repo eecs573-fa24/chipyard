5de27899c435c7d55aab9dc328224cec027fb069
Richard Wang <slytherinheir1214@gmail.com>
2024-11-25 08:43:13 -0500

    Working on running baseline

--- tests/hello.c
+++ tests/hello.c
@@ -2,10 +2,16 @@
 #include <riscv-pk/encoding.h>
 #include "marchid.h"
 #include <stdint.h>
+#include <malloc.h>
 
 int main(void) {
   uint64_t marchid = read_csr(marchid);
   const char* march = get_march(marchid);
   printf("Hello world from core 0, a %s\n", march);
+
+  struct mallinfo mi = mallinfo();
+  printf("Total heap space: %d bytes\n", mi.arena);
+  printf("Free heap space: %d bytes\n", mi.fordblks);
+  
   return 0;
 }
--- tests/lzw.c
+++ tests/lzw.c
@@ -2,43 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include <unistd.h>
-#include <limits.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-/* -------- aux stuff ---------- */
-void* mem_alloc(size_t item_size, size_t n_item)
-{
-	size_t *x = calloc(1, sizeof(size_t)*2 + n_item * item_size);
-	x[0] = item_size;
-	x[1] = n_item;
-	return x + 2;
-}
-
-void* mem_extend(void *m, size_t new_n)
-{
-	size_t *x = (size_t*)m - 2;
-	x = realloc(x, sizeof(size_t) * 2 + *x * new_n);
-	if (new_n > x[1])
-		memset((char*)(x + 2) + x[0] * x[1], 0, x[0] * (new_n - x[1]));
-	x[1] = new_n;
-	return x + 2;
-}
-
-inline void _clear(void *m)
-{
-	size_t *x = (size_t*)m - 2;
-	memset(m, 0, x[0] * x[1]);
-}
-
-#define _new(type, n) mem_alloc(sizeof(type), n)
-#define _del(m)   { free((size_t*)(m) - 2); m = 0; }
-#define _len(m)   *((size_t*)m - 1)
-#define _setsize(m, n)  m = mem_extend(m, n)
-#define _extend(m)  m = mem_extend(m, _len(m) * 2)
-
 
 /* ----------- LZW stuff -------------- */
 typedef uint8_t byte;
@@ -48,216 +11,292 @@ typedef uint16_t ushort;
 #define M_EOD 257 /* end-of-data marker */
 #define M_NEW 258 /* new code index */
 
-/* 	encode and decode dictionary structures.
-	for encoding, entry at code index is a list of indices that follow current one,
-	i.e. if code 97 is 'a', code 387 is 'ab', and code 1022 is 'abc',
-	then dict[97].next['b'] = 387, dict[387].next['c'] = 1022, etc. 
-*/
+/* Encode and decode dictionary structures */
 typedef struct {
-	ushort next[256];
+    ushort next[256];
 } lzw_enc_t;
 
-/* for decoding, dictionary contains index of whatever prefix index plus trailing
-	byte.  i.e. like previous example,
-	dict[1022] = { c: 'c', prev: 387 },
-	dict[387]  = { c: 'b', prev: 97 },
-	dict[97]   = { c: 'a', prev: 0 }
-	the "back" element is used for temporarily chaining indices when resolving
-	a code to bytes
- */
 typedef struct {
-	ushort prev, back;
-	byte c;
+    ushort prev, back;
+    byte c;
 } lzw_dec_t;
 
-byte* lzw_encode(byte *in, int max_bits)
-{
-	int len = _len(in), bits = 9, next_shift = 512;
-	ushort code, c, nc, next_code = M_NEW;
-  lzw_enc_t *d = _new(lzw_enc_t, 512);
+byte* lzw_encode(byte *in, int len, int max_bits, int *out_len) {
+    int bits = 9;
+    int next_shift = 512;
 
-	if (max_bits > 15) max_bits = 15;
-	if (max_bits < 9 ) max_bits = 12;
+    ushort code, c, nc, next_code = M_NEW;
 
-	byte *out = _new(ushort, 4);
-	int out_len = 0, o_bits = 0;
-	uint32_t tmp = 0;
+    if (max_bits > 15) max_bits = 15;
+    if (max_bits < 9 ) max_bits = 12;
 
-	inline void write_bits(ushort x) {
-		tmp = (tmp << bits) | x;
-		o_bits += bits;
-		if (_len(out) <= out_len) _extend(out);
-		while (o_bits >= 8) {
-		o_bits -= 8;
-		out[out_len++] = tmp >> o_bits;
-		tmp &= (1 << o_bits) - 1;
-		}
-	}
+    /* Initialize encoding dictionary */
+    int dict_size = 512;
+    lzw_enc_t *d = calloc(dict_size, sizeof(lzw_enc_t));
+    if (!d) {
+        fprintf(stderr, "Failed to allocate encoding dictionary\n");
+        return NULL;
+    }
 
-  //write_bits(M_CLR);
-	for (code = *(in++); --len; ) {
-		c = *(in++);
-		if ((nc = d[code].next[c]))
-		code = nc;
-		else {
-		write_bits(code);
-		nc = d[code].next[c] = next_code++;
-		code = c;
-		}
+    /* Initialize output buffer */
+    int out_size = 512;
+    byte *out = malloc(out_size * sizeof(byte));
+    if (!out) {
+        fprintf(stderr, "Failed to allocate output buffer\n");
+        free(d);
+        return NULL;
+    }
+    int out_len_internal = 0, o_bits = 0;
+    uint32_t tmp = 0;
 
-		/* next new code would be too long for current table */
-		if (next_code == next_shift) {
-		/* either reset table back to 9 bits */
-		if (++bits > max_bits) {
-			/* table clear marker must occur before bit reset */
-			write_bits(M_CLR);
+    void write_bits(ushort x) {
+        tmp = (tmp << bits) | x;
+        o_bits += bits;
+        while (o_bits >= 8) {
+            o_bits -= 8;
+            if (out_len_internal >= out_size) {
+                out_size *= 2;
+                out = realloc(out, out_size * sizeof(byte));
+                if (!out) {
+                    fprintf(stderr, "Failed to reallocate output buffer\n");
+                    free(d);
+                    exit(1);
+                }
+            }
+            out[out_len_internal++] = (tmp >> o_bits) & 0xFF;
+            tmp &= (1 << o_bits) - 1;
+        }
+    }
 
-			bits = 9;
-			next_shift = 512;
-			next_code = M_NEW;
-			_clear(d);
-		} else  /* or extend table */
-			_setsize(d, next_shift *= 2);
-		}
-	}
+    /* Start LZW encoding */
+    code = *(in++);
+    len--;
 
-	write_bits(code);
-	write_bits(M_EOD);
-	if (tmp) write_bits(tmp);
+    while (len--) {
+        c = *(in++);
+        if ((nc = d[code].next[c])) {
+            code = nc;
+        } else {
+            write_bits(code);
+            nc = d[code].next[c] = next_code++;
+            code = c;
+        }
 
-	_del(d);
+        if (next_code == next_shift) {
+            if (++bits > max_bits) {
+                write_bits(M_CLR);
 
-	_setsize(out, out_len);
-	return out;
+                bits = 9;
+                next_shift = 512;
+                next_code = M_NEW;
+                memset(d, 0, dict_size * sizeof(lzw_enc_t));
+            } else {
+                next_shift *= 2;
+                dict_size = next_shift;
+                d = realloc(d, dict_size * sizeof(lzw_enc_t));
+                if (!d) {
+                    fprintf(stderr, "Failed to reallocate encoding dictionary\n");
+                    free(out);
+                    return NULL;
+                }
+                memset(d + (dict_size / 2), 0, (dict_size / 2) * sizeof(lzw_enc_t));
+            }
+        }
+    }
+
+    write_bits(code);
+    write_bits(M_EOD);
+
+    if (o_bits > 0) {
+        if (out_len_internal >= out_size) {
+            out_size *= 2;
+            out = realloc(out, out_size * sizeof(byte));
+            if (!out) {
+                fprintf(stderr, "Failed to reallocate output buffer\n");
+                free(d);
+                return NULL;
+            }
+        }
+        out[out_len_internal++] = (tmp << (8 - o_bits)) & 0xFF;
+    }
+
+    free(d);
+
+    out = realloc(out, out_len_internal * sizeof(byte));
+    if (!out) {
+        fprintf(stderr, "Failed to reallocate output buffer to final size\n");
+        return NULL;
+    }
+
+    *out_len = out_len_internal;
+    return out;
 }
 
-byte* lzw_decode(byte *in)
-{
-	byte *out = _new(byte, 4);
-	int out_len = 0;
+byte* lzw_decode(byte *in, int len, int *out_len) {
+    byte *out = malloc(256 * sizeof(byte));
+    if (!out) {
+        fprintf(stderr, "Failed to allocate output buffer\n");
+        return NULL;
+    }
+    int out_size = 256;
+    int out_len_internal = 0;
 
-	inline void write_out(byte c)
-	{
-		while (out_len >= _len(out)) _extend(out);
-		out[out_len++] = c;
-	}
+    void write_out(byte c) {
+        if (out_len_internal >= out_size) {
+            out_size *= 2;
+            out = realloc(out, out_size * sizeof(byte));
+            if (!out) {
+                fprintf(stderr, "Failed to reallocate output buffer\n");
+                exit(1);
+            }
+        }
+        out[out_len_internal++] = c;
+    }
 
-	lzw_dec_t *d = _new(lzw_dec_t, 512);
-	int len, j, next_shift = 512, bits = 9, n_bits = 0;
-	ushort code, c, t, next_code = M_NEW;
+    int dict_size = 512;
+    lzw_dec_t *d = malloc(dict_size * sizeof(lzw_dec_t));
+    if (!d) {
+        fprintf(stderr, "Failed to allocate decoding dictionary\n");
+        free(out);
+        return NULL;
+    }
 
-	uint32_t tmp = 0;
-	inline void get_code() {
-		while(n_bits < bits) {
-		if (len > 0) {
-			len --;
-			tmp = (tmp << 8) | *(in++);
-			n_bits += 8;
-		} else {
-			tmp = tmp << (bits - n_bits);
-			n_bits = bits;
-		}
-		}
-		n_bits -= bits;
-		code = tmp >> n_bits;
-		tmp &= (1 << n_bits) - 1;
-	}
+    int j, next_shift = 512, bits = 9, n_bits = 0;
+    ushort code, c, t, next_code = M_NEW;
 
-	inline void clear_table() {
-		_clear(d);
-		for (j = 0; j < 256; j++) d[j].c = j;
-		next_code = M_NEW;
-		next_shift = 512;
-		bits = 9;
-	};
+    uint32_t tmp = 0;
+    void get_code() {
+        while(n_bits < bits) {
+            if (len > 0) {
+                len--;
+                tmp = (tmp << 8) | *(in++);
+                n_bits += 8;
+            } else {
+                tmp = tmp << (bits - n_bits);
+                n_bits = bits;
+            }
+        }
+        n_bits -= bits;
+        code = tmp >> n_bits;
+        tmp &= (1 << n_bits) -1;
+    }
 
-	clear_table(); /* in case encoded bits didn't start with M_CLR */
-	for (len = _len(in); len;) {
-		get_code();
-		if (code == M_EOD) break;
-		if (code == M_CLR) {
-		clear_table();
-		continue;
-		}
+    void clear_table() {
+        memset(d, 0, dict_size * sizeof(lzw_dec_t));
+        for (j = 0; j < 256; j++)
+            d[j].c = j;
+        next_code = M_NEW;
+        next_shift = 512;
+        bits = 9;
+    };
 
-		if (code >= next_code) {
-		fprintf(stderr, "Bad sequence\n");
-		_del(out);
-		goto bail;
-		}
+    clear_table();
+    while (len > 0 || n_bits >= bits) {
+        get_code();
+        if (code == M_EOD) break;
+        if (code == M_CLR) {
+            clear_table();
+            continue;
+        }
 
-		d[next_code].prev = c = code;
-		while (c > 255) {
-		t = d[c].prev; d[t].back = c; c = t;
-		}
+        if (code >= next_code) {
+            fprintf(stderr, "Bad sequence\n");
+            free(out);
+            free(d);
+            return NULL;
+        }
 
-		d[next_code - 1].c = c;
+        d[next_code].prev = c = code;
+        while (c > 255) {
+            t = d[c].prev; d[t].back = c; c = t;
+        }
 
-		while (d[c].back) {
-		write_out(d[c].c);
-		t = d[c].back; d[c].back = 0; c = t;
-		}
-		write_out(d[c].c);
+        d[next_code - 1].c = c;
 
-		if (++next_code >= next_shift) {
-		if (++bits > 16) {
-			/* if input was correct, we'd have hit M_CLR before this */
-			fprintf(stderr, "Too many bits\n");
-			_del(out);
-			goto bail;
-		}
-		_setsize(d, next_shift *= 2);
-		}
-	}
+        while (d[c].back) {
+            write_out(d[c].c);
+            t = d[c].back; d[c].back = 0; c = t;
+        }
+        write_out(d[c].c);
 
-	/* might be ok, so just whine, don't be drastic */
-	if (code != M_EOD) fputs("Bits did not end in EOD\n", stderr);
+        if (++next_code >= next_shift) {
+            if (++bits > 16) {
+                fprintf(stderr, "Too many bits\n");
+                free(out);
+                free(d);
+                return NULL;
+            }
+            next_shift *= 2;
+            dict_size = next_shift;
+            d = realloc(d, dict_size * sizeof(lzw_dec_t));
+            if (!d) {
+                fprintf(stderr, "Failed to reallocate decoding dictionary\n");
+                free(out);
+                return NULL;
+            }
+            memset(d + (dict_size / 2), 0, (dict_size / 2) * sizeof(lzw_dec_t));
+        }
+    }
 
-	_setsize(out, out_len);
-	bail: _del(d);
-	return out;
+    if (code != M_EOD)
+        fputs("Bits did not end in EOD\n", stderr);
+
+    out = realloc(out, out_len_internal * sizeof(byte));
+    if (!out) {
+        fprintf(stderr, "Failed to reallocate output buffer to final size\n");
+        free(d);
+        return NULL;
+    }
+
+    *out_len = out_len_internal;
+    free(d);
+    return out;
 }
 
-int main(int argc, char **argv)
-{
+int main() {
+    size_t i;
+	char *input = "LZW319274";
+    int in_len = strlen(input);
+    byte *in = malloc(in_len * sizeof(byte));
+    memcpy(in, input, in_len);
 
-	fprintf(stdout, argv[0]);
+    printf("input: %s\n", in);
+    printf("input size:   %d\n", in_len);
 
-	int i, fd = open("test.txt", O_RDONLY);
+    int enc_len;
+    byte *enc = lzw_encode(in, in_len, 9, &enc_len);
+    if (!enc) {
+        fprintf(stderr, "Encoding failed\n");
+        free(in);
+        return 1;
+    }
+    printf("encoded size: %d\n", enc_len);
+	printf("encoded string: %s\n", enc);
 
-	if (fd == -1) {
-		fprintf(stderr, "Can't read file\n");
-		return 1;
-	};
+    int dec_len;
+    byte *dec = lzw_decode(enc, enc_len, &dec_len);
+    if (!dec) {
+        fprintf(stderr, "Decoding failed\n");
+        free(in);
+        free(enc);
+        return 1;
+    }
+    printf("decoded size: %d\n", dec_len);
 
-	struct stat st;
-	fstat(fd, &st);
+    for (i = 0; i < dec_len; i++)
+        if (dec[i] != in[i]) {
+            printf("bad decode at %zu\n", i);
+            break;
+        }
 
-	byte *in = _new(char, st.st_size);
-	read(fd, in, st.st_size);
-	_setsize(in, st.st_size);
-	close(fd);
+    if (i == dec_len) {
+        printf("Decoded ok\n");
+        printf("decoded string: %.*s\n", dec_len, dec);
+    }
 
-	printf("input size:   %d\n", _len(in));
+    free(in);
+    free(enc);
+    free(dec);
 
-	byte *enc = lzw_encode(in, 9);
-	printf("encoded size: %d\n", _len(enc));
-
-	byte *dec = lzw_decode(enc);
-	printf("decoded size: %d\n", _len(dec));
-
-	for (i = 0; i < _len(dec); i++)
-		if (dec[i] != in[i]) {
-		printf("bad decode at %d\n", i);
-		break;
-		}
-
-	if (i == _len(dec)) printf("Decoded ok\n");
-
-
-	_del(in);
-	_del(enc);
-	_del(dec);
-
-	return 0;
+    return 0;
 }
